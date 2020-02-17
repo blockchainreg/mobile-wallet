53605bdaf4259cc89f16b2b3f7e1675181ee63dc
Dmitry Okhrimenko <dmytro.okhrimenko@gmail.com>
2020-02-17 22:49:36 +0200

    Dots title

--- pages/AddWallets.js
+++ pages/AddWallets.js
@@ -22,82 +22,73 @@ import Toast from "@rimiti/react-native-toastify";
 // import StatusBar from "../components/StatusBar.js";
 
 //
-import dash from '../registry/dash.json';
-import etc from '../registry/etc.json';
-import eth from '../registry/eth.json';
-import ltc from '../registry/ltc.json';
-import usdt from '../registry/usdt.json';
-import usdt_erc20 from '../registry/usdt_erc20.json';
-import walletsFuncs from '../wallet/wallets-funcs.js';
+import dash from "../registry/dash.json";
+import etc from "../registry/etc.json";
+import eth from "../registry/eth.json";
+import ltc from "../registry/ltc.json";
+import usdt from "../registry/usdt.json";
+import usdt_erc20 from "../registry/usdt_erc20.json";
+import walletsFuncs from "../wallet/wallets-funcs.js";
 //
 
-const coinItems = [ dash, etc, eth, ltc, usdt, usdt_erc20 ];
+const coinItems = [dash, etc, eth, ltc, usdt, usdt_erc20];
 
+const renderCoin = (store, web3t) => item => {
+  const tokens = walletsFuncs(store, web3t).wallets.map(x => x.coin.token);
 
-const renderCoin = (store, web3t) => (item) => {
+  const addItem = () => {
+    store.current.loading = true;
+    web3t.installQuick(item, (err, data) => {
+      //console.log("install", err, data);
+      //store.current.page = "wallets";
+      store.current.loading = false;
+    });
+  };
 
-     const tokens = walletsFuncs(store, web3t).wallets.map((x) => x.coin.token);
+  const deleteItem = () => {
+    store.current.loading = true;
+    web3t.uninstall(item.token, (err, data) => {
+      //store.current.page = "wallets";
+      store.current.loading = false;
+    });
+  };
 
-     const addItem = () => {
-        store.current.loading = true;
-        web3t.installQuick(item, (err, data) => {
-            //console.log("install", err, data);
-            //store.current.page = "wallets";
-            store.current.loading = false;
-        })
-     }
+  const isAdded = tokens.indexOf(item.token) > -1;
 
-     const deleteItem = () => {
-        store.current.loading = true;
-        web3t.uninstall(item.token, (err, data) => {
-            //store.current.page = "wallets";
-            store.current.loading = false;
-        })  
-     }
+  const currentAction = isAdded ? deleteItem : addItem;
+  const currentIcon = isAdded ? "ios-remove" : "ios-add";
 
-     const isAdded = tokens.indexOf(item.token) > -1;
-
-     const currentAction = isAdded ? deleteItem : addItem;
-     const currentIcon = isAdded ? "ios-remove" : "ios-add";
-
-
-     return (
-      <ListItem
-          thumbnail
-          style={styles.mbListItem}
-          onPress={currentAction}
-          key={item.token}
-        >
-          <Left>
-            <Thumbnail source={{uri: item.image}} />
-          </Left>
-          <Body>
-          <Text note/>
-            <Text style={styles.amountView}>
-              {item.token.toUpperCase()}
-            </Text>
-            <Text note/>
-          </Body>
-          <Right>
-            <Button transparent onPress={currentAction}>
-              <Icon name={currentIcon} style={styles.iconBtn} />
-            </Button>
-          </Right>
-        </ListItem>
-    )
-  }
-
-
+  return (
+    <ListItem
+      thumbnail
+      style={styles.mbListItem}
+      onPress={currentAction}
+      key={item.token}
+    >
+      <Left>
+        <Thumbnail source={{ uri: item.image }} />
+      </Left>
+      <Body>
+        <Text note />
+        <Text style={styles.amountView}>{item.token.toUpperCase()}</Text>
+        <Text note />
+      </Body>
+      <Right>
+        <Button transparent onPress={currentAction}>
+          <Icon name={currentIcon} style={styles.iconBtn} />
+        </Button>
+      </Right>
+    </ListItem>
+  );
+};
 
 export default ({ store, web3t }) => {
-  const changePage = (tab) => () => {
+  const changePage = tab => () => {
     store.current.page = tab;
   };
 
   const calcUsd = 0;
-  const refreshToken = async bool => {
-
-  };
+  const refreshToken = async bool => {};
   const checkType = type => {
     switch (type) {
       case "USD":
@@ -113,15 +104,18 @@ export default ({ store, web3t }) => {
       <StandardLinearGradient>
         <RefreshControl swipeRefresh={refreshToken}>
           <Header transparent style={styles.mtIphoneX}>
-          <Left style={styles.viewFlex}>
-            <Button
-              transparent
-              style={styles.arrowHeaderLeft}
-              onPress={changePage("wallets")}
-            >
-              <Icon name="ios-arrow-back" style={[styles.arrowHeaderIconBlack, {color: '#fff'}]} />
-            </Button>
-          </Left>
+            <Left style={styles.viewFlex}>
+              <Button
+                transparent
+                style={styles.arrowHeaderLeft}
+                onPress={changePage("wallets")}
+              >
+                <Icon
+                  name="ios-arrow-back"
+                  style={[styles.arrowHeaderIconBlack, { color: "#fff" }]}
+                />
+              </Button>
+            </Left>
             <Body style={styles.viewFlex}>
               <Text style={styles.title1}>Manage Wallets</Text>
             </Body>
@@ -135,9 +129,7 @@ export default ({ store, web3t }) => {
         <View style={styles.viewPt} />
         <ScrollView>
           <Content>
-            <List>
-              {coinItems.map(renderCoin(store, web3t))}
-            </List>
+            <List>{coinItems.map(renderCoin(store, web3t))}</List>
           </Content>
         </ScrollView>
       </View>
