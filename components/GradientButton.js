981848ecf06d76f4e22c04e0ddc0307fe0a6ce40
Kostya Pogromskiy <kpogromskiy@gmail.com>
2020-07-10 20:50:48 +0300

    More fixes to gradient button

--- components/GradientButton.js
+++ components/GradientButton.js
@@ -1,7 +1,8 @@
 import React from "react";
 import GradientButton from "react-native-gradient-buttons";
-// GradientButton truncated last symbol on some phones. That is why we add space here.
+// GradientButton truncated last symbol(s) on some phones. That is why we add spaces here.
+// It also truncate by word boundary so replace spaces to nbsp.
 export default (props) => <GradientButton
   {...props }
-  text={props.text + " "}
+  text={(props.text + '   ').split(' ').join('\u00A0')}
 />;
