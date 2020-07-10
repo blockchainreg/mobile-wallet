3ee892147e63ccde979f7b142a5f2e3088887d91
Kostya Pogromskiy <kpogromskiy@gmail.com>
2020-07-10 15:26:50 +0300

    Save seed on restore

--- localStorage.js
+++ localStorage.js
@@ -13,18 +13,28 @@ async function makeProxy() {
     inMemoryStorage[key] = value;
   }
   function setItem(key, value) {
+    // console.log('setItem', key, value);
     inMemoryStorage[key] = value;
     AsyncStorage.setItem(key, value);
   }
   function clear() {
+    // console.log('Clear');
     for (let key in inMemoryStorage) {
       delete inMemoryStorage[key];
     }
     return AsyncStorage.clear();
   }
   function removeItem(key) {
+    // console.log('removeItem', key);
     delete inMemoryStorage[key];
     AsyncStorage.removeItem(key);
+  }
+  function getItem(key) {
+    if (prototype[key]) {
+      return prototype[key];
+    }
+    // console.log('getItem', key, inMemoryStorage[key]);
+    return inMemoryStorage[key];
   }
   const prototype = {
     getItem: (key) => inMemoryStorage[key],
@@ -34,7 +44,7 @@ async function makeProxy() {
   };
 
   const proxy = new Proxy(inMemoryStorage, {
-    get: (target, key) => prototype[key] || target[key],
+    get: (target, key) => getItem(key),
     set: (target, key, value) => setItem(key, value)
   });
   global.localStorage = proxy;
