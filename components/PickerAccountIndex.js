d3f423278a2c6f4ca4bf70c43bad2dc973f8c274
Kostya Pogromskiy <kpogromskiy@gmail.com>
2020-07-21 19:29:28 +0300

    Select account is now input

--- components/PickerAccountIndex.js
+++ components/PickerAccountIndex.js
@@ -1,50 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import RNPickerSelect from "react-native-picker-select";
 import { Ionicons } from '@expo/vector-icons';
 import { KECCAK256_NULL } from "ethereumjs-util";
+import {
+  Input,
+  Item,
+} from "native-base";
 
 import spin from "../utils/spin.js";
 
+const InputComponent = ({accountIndex, onValueChange}) => {
+  const [index, indexChange] = useState(accountIndex);
+  const onBlur = () => {
+    let value = parseInt(index);
+    if (isNaN(value)) {
+      value = 1;
+    }
+    indexChange(value + "");
+    onValueChange(value);
+  };
+  return (
+      <Input
+        value={index + ""}
+        onChangeText={indexChange}
+        onBlur={onBlur}
+        keyboardType="numeric"
+        style={{
+          color: "#fff",
+          fontSize: 17,
+        }}
+      />
+  );
+}
+
 export default ({ store, web3t }) => {
-  const onValueChangeValue = async value => {
+  const onValueChange = (value) => {
     //store.lang = value;
     //localStorage.setItem("lang", value);
+    if (store.current.accountIndex === value) {
+      return;
+    }
     store.current.accountIndex = value;
     spin(store, `Updating account index`, web3t.refresh.bind(web3t))((err,data) => {
         console.log("Updating account index finished");
     })
   };
-  const createIndex = (i)=> {
-     return {
-      label: "Account " + i,
-      value: i
-    }
-  }
-  const langItems = [...Array(1000).keys()].map(createIndex)
-
-  return (
-    <RNPickerSelect
-      placeholder={{}}
-      onValueChange={value => {
-        onValueChangeValue(value);
-      }}
-      useNativeAndroidPickerStyle={false}
-      value={store.current.accountIndex}
-      items={langItems}
-      useNativeAndroidPickerStyle={false}
-      Icon={() => {
-        return <Ionicons name="md-arrow-down" size={24} color="transparent" />;
-      }}
-      style={{
-        inputIOS: {
-          color: "#fff",
-          fontSize: 17,
-        },
-        inputAndroid: {
-          color: "#fff",
-          fontSize: 17,
-        }
-      }}
-    />
-  );
+  // const createIndex = (i)=> {
+  //    return {
+  //     label: "Account " + i,
+  //     value: i
+  //   }
+  // }
+  return <InputComponent accountIndex={store.current.accountIndex} onValueChange={onValueChange}/>;
+  // const langItems = [...Array(1000).keys()].map(createIndex)
+  //
+  // return (
+  //   <RNPickerSelect
+  //     placeholder={{}}
+  //     onValueChange={value => {
+  //       onValueChangeValue(value);
+  //     }}
+  //     useNativeAndroidPickerStyle={false}
+  //     value={store.current.accountIndex}
+  //     items={langItems}
+  //     useNativeAndroidPickerStyle={false}
+  //     Icon={() => {
+  //       return <Ionicons name="md-arrow-down" size={24} color="transparent" />;
+  //     }}
+  //     style={{
+  //       inputIOS: {
+  //         color: "#fff",
+  //         fontSize: 17,
+  //       },
+  //       inputAndroid: {
+  //         color: "#fff",
+  //         fontSize: 17,
+  //       }
+  //     }}
+  //   />
+  // );
 };
