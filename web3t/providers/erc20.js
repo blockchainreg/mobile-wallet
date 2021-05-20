cf1ee62bdc8c447e09ef656573f51db85348706b
Artem Prushchik <apindesign@gmail.com>
2021-05-20 18:31:06 +0300

    Catch errors in contract` balanceOf method

--- web3t/providers/erc20.js
+++ web3t/providers/erc20.js
@@ -388,10 +388,10 @@
         feeType: feeType
       }, function(err, gasPriceBn){
         var gasPrice, gasMinimal, gasEstimate;
-        gasPrice = gasPriceBn.toFixed();
         if (err != null) {
           return cb(err);
         }
+				gasPrice = gasPriceBn.toFixed();
         gasMinimal = div(toWeiEth(amountFee), gasPrice);
         gasEstimate = round(times(gasMinimal, 5));
         if (toString$.call(web3.eth.getBalance).slice(8, -1) !== 'Function') {
@@ -501,6 +501,7 @@
     network = arg$.network, address = arg$.address;
     web3 = getWeb3(network);
     contract = getContractInstance(web3, network.address);
+    console.log(contract.balanceOf);
     balanceOf = (function(){
       switch (false) {
       case contract.methods == null:
@@ -513,15 +514,20 @@
         };
       }
     }());
-    return balanceOf(address, function(err, number){
-      var dec, balance;
-      if (err != null) {
-        return cb(err);
-      }
-      dec = getDec(network);
-      balance = div(number, dec);
-      return cb(null, balance);
-    });
+    try {
+			return balanceOf(address, function (err, number) {
+				var dec, balance;
+				if (err != null) {
+					return cb(err);
+				}
+				dec = getDec(network);
+				balance = div(number, dec);
+				return cb(null, balance);
+			});
+		} catch (e) {
+    	console.log("Caught error, balanceOf:", e);
+			return cb(null, "..");
+		}
   };
   out$.isValidAddress = isValidAddress = function(arg$, cb){
 	var address, network, valid;
