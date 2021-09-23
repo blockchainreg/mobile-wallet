964f32926144fc3b068a7bd0ade962736e01f752
Artem <apindesign@gmail.com>
2021-09-23 20:55:42 +0300

    callinitStaking when network change

--- components/PickerSetNetwork.js
+++ components/PickerSetNetwork.js
@@ -2,12 +2,19 @@ import React from "react";
 import { Alert } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 import spin from "../utils/spin.js";
+import initStaking from '../initStaking.js';
+import {Toast} from "native-base";
 
 export default ({ store, web3t }) => {
   const onValueChangeValue = async (value) => {
-    store.current.network = value;
-    await console.log("store.current.network", store.current.network);
-    return web3t.refresh(function () {});
+  	store.current.network = value;
+	initStaking(store);
+    spin(
+	  store,
+	  "Changing network...",
+		web3t.refresh.bind(web3t)
+    )((err, data) => {});
+	
   };
   const networkItems = [
     {
