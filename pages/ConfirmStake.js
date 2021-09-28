0e55e0502a3223fc3ae6c64da08c74ad147ebc1d
Artem <apindesign@gmail.com>
2021-09-29 00:34:17 +0300

    minor confirm swap text fix

--- pages/ConfirmStake.js
+++ pages/ConfirmStake.js
@@ -82,7 +82,7 @@ export default ({ store, web3t, props }) => {
               <StepItem
                 index="1"
                 // text={lang.stepItem1 || "Convert 1,000,000 VLX to VLX Native"}
-                text={`Convert ${formatStakeAmount(swapAmount)} VLX to VLX Native`}
+                text={`Convert ${formatStakeAmount(swapAmount)} VLX Legacy to VLX Native`}
                 address={stakingStore.publicKey58}
               />
               <StepItem
