de87e908ed0d889ec6fe742172db51fff5a1c34e
FrozenPyrozen <kiril.kohan@gmail.com>
2022-01-14 21:48:56 +0200

    add fast bsc testnet providers to top on binanceBscRpc

--- web3t/plugins/commonTokenConfigs/binanceBscRpc.js
+++ web3t/plugins/commonTokenConfigs/binanceBscRpc.js
@@ -23,13 +23,13 @@ const binanceBscRpc = Object.freeze({
   },
   testnet: {
     api: {
-      web3Provider: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      web3Provider: 'https://data-seed-prebsc-1-s2.binance.org:8545',
       extraWeb3Providers: [
         'https://data-seed-prebsc-2-s1.binance.org:8545',
-        'https://data-seed-prebsc-1-s2.binance.org:8545',
         'https://data-seed-prebsc-2-s2.binance.org:8545',
         'https://data-seed-prebsc-1-s3.binance.org:8545',
         'https://data-seed-prebsc-2-s3.binance.org:8545',
+        'https://data-seed-prebsc-1-s1.binance.org:8545',
       ],
     },
   },
