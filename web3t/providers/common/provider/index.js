66a986ee20469d6c87e7db0822018d7c9b8be16e
FrozenPyrozen <kiril.kohan@gmail.com>
2022-01-18 18:02:51 +0200

    added formatting address for `symblox` in [getBalance]

--- web3t/providers/common/provider/index.js
+++ web3t/providers/common/provider/index.js
@@ -1,6 +1,7 @@
 import { post } from '../../superagent';
 import ERC20BridgeToken from '../../../contracts/ERC20BridgeToken.json';
 import { div } from '../../../math';
+import { vlxToEth } from '../../../addresses';
 const ABI = [
   {
     constant: true,
@@ -381,7 +382,7 @@ const balanceOfWithAvailableWeb3Provider = function (
   const [web3Provider, ...extraWeb3Providers] = web3Providers;
 
   if (!web3Provider) {
-    retrun(cb(null));
+    return cb(null);
   }
 
   const web3 = getWeb3ByProvider(web3Provider);
@@ -421,7 +422,7 @@ const balanceOfWithAvailableWeb3Provider = function (
       return cb(null, balance);
     });
   } catch (err) {
-    console.error('[getBalance] err in cath', err);
+    console.error('[getBalance] err in catch', err);
     return cb(err);
   }
 };
@@ -432,7 +433,7 @@ const getBalance = (arg$, cb) => {
   const web3Providers = getWeb3Providers(web3Provider, extraWeb3Providers);
 
   return balanceOfWithAvailableWeb3Provider(
-    address,
+    !address.startsWith('V') ? address : vlxToEth(address),
     network,
     web3Providers,
     cb
