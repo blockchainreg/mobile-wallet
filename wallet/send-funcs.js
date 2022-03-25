88fdc3b209c834307d558731a19dc571fa8f1035
Artem P <apindesign@gmail.com>
2022-03-25 15:56:32 +0200

    bugfix: https://velasnetwork.atlassian.net/browse/VLWA-1361  native-legacy swap stuck

--- wallet/send-funcs.js
+++ wallet/send-funcs.js
@@ -332,11 +332,18 @@ import roundNumber from '../round-number';
         store.current.send.contractAddress != null
           ? store.current.send.contractAddress
           : transaction.recipient;
+      const chosenNetwork = store.current.send.chosenNetwork;
+      const referTo = chosenNetwork != null ? chosenNetwork.referTo : void 8;
       const recipient = (function () {
         switch (false) {
           case !(
-            store.current.send.coin.token !== 'vlx_native' &&
+            referTo != null &&
+            referTo !== 'vlx_native' &&
             receiver.startsWith('V')
+          ):
+            return toEthAddress(receiver);
+          case !(
+            transaction.coin.token !== 'vlx_native' && receiver.startsWith('V')
           ):
             return toEthAddress(receiver);
           default:
