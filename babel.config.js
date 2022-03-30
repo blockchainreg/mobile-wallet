0cadec7b1cba7080e7d8b6ee2023ca868694c7c9
FrozenPyrozen <kiril.kohan@gmail.com>
2022-03-30 17:43:29 +0300

    read `TERMS.md` from local file
    - added `babel-plugin-inline-import` and configured `babel` and `metro` configs

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,28 @@ module.exports = function (api) {
   api.cache(true);
 
   const plugins = [];
+  // For 'module:metro-react-native-babel-preset'
+  plugins.push(
+    [
+      'inline-import',
+      {
+        extensions: ['.md', '.txt'],
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {},
+      },
+    ]
+  );
   if (babelEnv !== 'development') {
     // Temporaty disabled for capturing logs for sentry crash reports
     // plugins.push(['transform-remove-console']);
   }
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
     plugins,
   };
 };
